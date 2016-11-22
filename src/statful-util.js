7b2ffce344074c16bbb75015fd126aec0c4d9be6
Jose Tavares <josetavares00@gmail.com>
2016-11-22 11:55:24 +0000

    feat: add sampleRate, simplify condition

--- src/statful-util.js
+++ src/statful-util.js
@@ -123,11 +123,9 @@
         this.addItemToQueue = function (queueName, item) {
             var sampleRateNormalized = (item.sampleRate || this.config.sampleRate || 100) / 100;
 
-            if (Math.random() <= sampleRateNormalized) {
-                if (this.listQueues[queueName]) {
-                    this.listQueues[queueName].data.push(item);
-                    return true;
-                }
+            if (this.listQueues[queueName] && Math.random() <= sampleRateNormalized) {
+                this.listQueues[queueName].data.push(item);
+                return true;
             } else {
                 logger.debug('Metric was discarded due to sample rate.');
                 return false;
