dd066eae0685cb553812173a5e0a85989cedfe77
Couto <hello@luiscouto.pt>
2017-07-18 20:48:21 +0100

    feat(NO-US): Add imports/exports to source files.
    Since rollup comes out of the box with support for imports/exports, we
    can better declare the required dependencies to make our code work.
    As a side-effect this allows us to remove global variables since we can
    new import values

--- src/logger.js
+++ src/logger.js
@@ -1,32 +1,27 @@
-(function (window) {
-    'use strict';
 
-    function Logger(enableDebug) {
-        this.debugEnabled = enableDebug || false;
+function Logger(enableDebug) {
+    this.debugEnabled = enableDebug || false;
+}
+
+Logger.prototype.info = function () {
+    if (this.debugEnabled) {
+        var args = Array.prototype.slice.call(arguments);
+        console.info.apply(console, args);
     }
+};
 
-    Logger.prototype.info = function () {
-        if (this.debugEnabled) {
-            var args = Array.prototype.slice.call(arguments);
-            console.info.apply(console, args);
-        }
-    };
+Logger.prototype.debug = function () {
+    if (this.debugEnabled) {
+        var args = Array.prototype.slice.call(arguments);
+        console.debug.apply(console, args);
+    }
+};
 
-    Logger.prototype.debug = function () {
-        if (this.debugEnabled) {
-            var args = Array.prototype.slice.call(arguments);
-            console.debug.apply(console, args);
-        }
-    };
+Logger.prototype.error = function () {
+    if (this.debugEnabled) {
+        var args = Array.prototype.slice.call(arguments);
+        console.error.apply(console, args);
+    }
+};
 
-    Logger.prototype.error = function () {
-        if (this.debugEnabled) {
-            var args = Array.prototype.slice.call(arguments);
-            console.error.apply(console, args);
-        }
-    };
-
-    window.StatfulLogger = Logger;
-
-})(window);
-
+export default Logger;
