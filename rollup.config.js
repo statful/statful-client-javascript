26edf7f9f5868153cf410eab51b5c9d7948b462b
Couto <hello@luiscouto.pt>
2017-07-19 01:18:50 +0100

    fix(NO-US): Force banner in all files

--- rollup.config.js
+++ rollup.config.js
@@ -4,41 +4,41 @@ import uglify from 'rollup-plugin-uglify';
 import license from 'rollup-plugin-license';
 
 const defaults = {
-  entry: 'src/statful.js',
-  moduleName: 'statful',
-  plugins: [
-    babel(),
-    license({
-      sourceMap: true,
-      banner: pkg.banner
-    }),
-  ],
-}
+    entry: 'src/statful.js',
+    moduleName: 'statful',
+    plugins: [
+        babel(),
+        license({
+            sourceMap: true,
+            banner: pkg.banner
+        }),
+    ],
+};
 
 export default [
-  // Browser friendly with UMD build
-  Object.assign({}, defaults, {
-    dest: pkg.browser,
-    format: 'umd',
-  }),
+    // Browser friendly with UMD build
+    Object.assign({}, defaults, {
+        dest: pkg.browser,
+        format: 'umd',
+    }),
 
-  // Browser friendly with UMD build minified
-  Object.assign({}, defaults, {
-    dest: 'dist/statful.umd.min.js',
-    format: 'umd',
-    plugins: defaults.plugins.concat(uglify())
-  }),
+    // Browser friendly with UMD build minified
+    Object.assign({}, defaults, {
+        dest: 'dist/statful.umd.min.js',
+        format: 'umd',
+        plugins: [uglify()].concat(defaults.plugins)
+    }),
 
-  // Browser not so friendly with global variable
-  Object.assign({}, defaults, {
-    dest: pkg.main,
-    format: 'iife',
-  }),
+    // Browser not so friendly with global variable
+    Object.assign({}, defaults, {
+        dest: pkg.main,
+        format: 'iife',
+    }),
 
-  // Browser not so friendly with global variable minified
-  Object.assign({}, defaults, {
-    dest: 'dist/statful.min.js',
-    format: 'iife',
-    plugins: defaults.plugins.concat(uglify())
-  }),
+    // Browser not so friendly with global variable minified
+    Object.assign({}, defaults, {
+        dest: 'dist/statful.min.js',
+        format: 'iife',
+        plugins: [uglify()].concat(defaults.plugins)
+    }),
 ];
